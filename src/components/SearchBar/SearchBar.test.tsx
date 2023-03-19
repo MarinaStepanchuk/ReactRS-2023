34ebeacd6c5933c5500a86e29c8dcfd13ee2bc4f
MarinaStepanchuk <106726117+MarinaStepanchuk@users.noreply.github.com>
2023-03-20 00:12:10 +0300

    feat: add function test

---
+++ src/components/SearchBar/SearchBar.test.tsx
@@ -0,0 +1,17 @@
+import SearchBar from './SearchBar';
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { vitest } from 'vitest';
+import userEvent from '@testing-library/user-event';
+
+describe('SearchBar', () => {
+  const onTextChange = vitest.fn();
+
+  it('should render correct', async () => {
+    const user = userEvent.setup();
+    render(<SearchBar text="" onTextChange={onTextChange} />);
+    await user.type(screen.getByRole('textbox'), '123');
+    expect(onTextChange).toHaveBeenCalled();
+    expect(onTextChange).toHaveBeenLastCalledWith('3');
+  });
+});
