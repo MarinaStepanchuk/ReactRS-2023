3660213757357de0eb97ed09f1f0cae41cfcd91c
MarinaStepanchuk <106726117+MarinaStepanchuk@users.noreply.github.com>
2023-04-04 20:10:55 +0300

    fix: save text before update

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,19 +1,32 @@
-import { Buttons } from '../../constants/common.constants';
+import { useEffect, useRef, useState } from 'react';
+import { Buttons, LocalStorageKeys } from '../../constants/common.constants';
 import classes from './SearchBar.module.scss';
 
 interface ISearchBarProps {
-  text: string;
   onTextChange: (value: string) => void;
 }
 
-const SearchBar = ({ text, onTextChange }: ISearchBarProps): JSX.Element => {
+const SearchBar = ({ onTextChange }: ISearchBarProps): JSX.Element => {
+  const [searchText, setSearchText] = useState(localStorage.getItem(LocalStorageKeys.search) || '');
+
+  const inputText = useRef(searchText);
+
   const handleTextChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    inputText.current = e.target.value;
+    setSearchText(e.target.value);
     onTextChange(e.target.value);
   };
 
+  useEffect(() => () => localStorage.setItem(LocalStorageKeys.search, `${inputText.current}`), []);
+
   return (
     <div className={classes.searchBar}>
-      <input className={classes.search} type="text" value={text} onChange={handleTextChange} />
+      <input
+        className={classes.search}
+        type="text"
+        value={searchText}
+        onChange={handleTextChange}
+      />
       <button className={classes.submit}>{Buttons.search}</button>
     </div>
   );
--- src/containers/Movies/Movies.tsx
+++ src/containers/Movies/Movies.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import SearchBar from '../../components/SearchBar/SearchBar';
 import defaultMoviesState from '../../constants/defaultStateMovies';
 import ListMovies from '../ListMovies/ListMovies';
@@ -6,19 +6,16 @@ import { LocalStorageKeys } from '../../constants/common.constants';
 
 const Movies = (): JSX.Element => {
   const movies = defaultMoviesState;
+  // let text = localStorage.getItem(LocalStorageKeys.search) || '';
   const [text, setText] = useState(localStorage.getItem(LocalStorageKeys.search) || '');
 
-  const handleTextChange = (text: string): void => {
-    setText(text);
+  const handleTextChange = (textV: string): void => {
+    setText(textV);
   };
-
-  useEffect(() => {
-    return () => localStorage.setItem(LocalStorageKeys.search, `${text}`);
-  });
 
   return (
     <section>
-      <SearchBar text={text} onTextChange={handleTextChange} />
+      <SearchBar onTextChange={handleTextChange} />
       <ListMovies text={text} movies={movies} />
     </section>
   );
