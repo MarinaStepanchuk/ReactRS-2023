168ff3f312943342ffaa2e5bd5cf7a0d9b1350b0
MarinaStepanchuk <106726117+MarinaStepanchuk@users.noreply.github.com>
2023-04-17 18:37:17 +0300

    fix: remove library for testing fetch

--- src/components/ItemMovie/ItemMovie.test.tsx
+++ src/components/ItemMovie/ItemMovie.test.tsx
@@ -1,21 +1,9 @@
 import ItemMovie from './ItemMovie';
 import { fireEvent, render, screen, waitFor } from '@testing-library/react';
-import { rest } from 'msw';
-import { setupServer } from 'msw/node';
 import movieMock from '../../mocks/movieMock';
 import movieDetailsMock from '../../mocks/movieDetailsMock';
 import { Provider } from 'react-redux';
 import { store } from '../../redux/store/store';
-
-const server = setupServer(
-  rest.get(`https://api.kinopoisk.dev/v1/movie/${movieDetailsMock.id}`, (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(movieDetailsMock));
-  })
-);
-
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
 
 describe('Movies', () => {
   it('should send fetch and render modal card', async () => {
