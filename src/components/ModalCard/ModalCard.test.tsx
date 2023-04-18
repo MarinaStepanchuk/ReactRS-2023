2ead092a0e6ed8ebc846c20c80c4c1cdb735a6c9
MarinaStepanchuk <106726117+MarinaStepanchuk@users.noreply.github.com>
2023-04-18 16:33:54 +0300

    feat: add test modal card

---
+++ src/components/ModalCard/ModalCard.test.tsx
@@ -0,0 +1,26 @@
+import ModalCard from './ModalCard';
+import { render, screen } from '@testing-library/react';
+import movieMock from '../../mocks/movieMock';
+import { useGetMovieByIdQuery } from '../../redux/services/MoviesService';
+import { useAppSelector } from '../../hooks/redux';
+import { vitest, Mock } from 'vitest';
+import movieDetailsMock from '../../mocks/movieDetailsMock';
+
+vitest.mock('../../redux/services/MoviesService');
+vitest.mock('../../hooks/redux');
+
+describe('ModalCard', () => {
+  it('should render modal card', async () => {
+    (useGetMovieByIdQuery as Mock).mockReturnValue({
+      data: movieDetailsMock,
+    });
+
+    (useAppSelector as Mock).mockReturnValue({
+      idCard: movieDetailsMock.id,
+    });
+
+    render(<ModalCard />);
+
+    expect(screen.getByText(movieDetailsMock.description, { exact: false })).toBeInTheDocument();
+  });
+});
